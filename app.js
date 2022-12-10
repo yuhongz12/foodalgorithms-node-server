3eb761fb22a94940a82293e921cffc2ae36a0fb9
Jenny <dolientrang@gmail.com>
2022-12-10 14:15:24 -0500

    update

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ import express from 'express'
 import cors from 'cors'
 import MealDBController from './mealdb/mealdb-controller.js'
 import UsersController from "./users/users-controller.js";
+import mongoose from "mongoose";
+
+const CONNECTION_STRING = 'mongodb://localhost:27017/foodalgorithms'
+//                           || process.env.DB_CONNECTION_STRING
+mongoose.connect(CONNECTION_STRING);
+
 const app = express()
 app.use(cors())
 app.use(express.json())
