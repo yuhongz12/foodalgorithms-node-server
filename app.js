00ae100a9cddb09089c7f55d59fcfa1522066b47
Jenny <dolientrang@gmail.com>
2022-12-10 14:36:43 -0500

    update

--- app.js
+++ app.js
@@ -8,8 +8,17 @@ const CONNECTION_STRING = 'mongodb://localhost:27017/foodalgorithms'
 //                           || process.env.DB_CONNECTION_STRING
 mongoose.connect(CONNECTION_STRING);
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors({
+                 credentials: true,
+                 origin: 'http://localhost:3000'
+             }))
+app.use(session({
+                    secret: 'should be an environment variable',
+                    resave: false,
+                    saveUninitialized: true,
+                    cookie: { secure: false }
+                }))
 app.use(express.json())
 
 MealDBController(app)
